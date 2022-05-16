22aeb35cdc4726d10dec34e68c18cfb68524ba10
Ganmodokix <lugerp07@live.jp>
2022-05-16 12:17:52 +0900

    fix fid

--- vaetc/evaluation/metrics/__init__.py
+++ vaetc/evaluation/metrics/__init__.py
@@ -3,5 +3,6 @@ from . import dci
 from . import intervention
 from . import predictor
 from . import elbo
+from . import generation
 
 from .save import evaluate
--- vaetc/evaluation/metrics/generation/fid.py
+++ vaetc/evaluation/metrics/generation/fid.py
@@ -13,6 +13,25 @@ from tqdm import tqdm
 from vaetc.utils.debug import debug_print
 
 @torch.no_grad()
+def features_batch(x: torch.Tensor, features, transform) -> torch.Tensor:
+
+    
+    x = transform(x)
+    x = x.cuda()
+
+    x = features._transform_input(x)
+    f = features(x)
+
+    return f.detach()
+
+def mean_and_cov(f: torch.Tensor) -> tuple[np.ndarray, np.ndarray]:
+
+    mean = f.mean(dim=0)
+    cov = f.T.cov()
+
+    return mean.detach().cpu().numpy(), cov.detach().cpu().numpy()
+
+@torch.no_grad()
 def features_mean_and_cov(x_data: np.ndarray, features, transform, batch_size=64) -> tuple[np.ndarray, np.ndarray]:
 
     fs = []
@@ -22,37 +41,15 @@ def features_mean_and_cov(x_data: np.ndarray, features, transform, batch_size=64
         x = x_data[ib:ib+batch_size]
         x = torch.tensor(x, dtype=torch.float)
         
-        x = transform(x)
-        x = x.cuda()
-
-        x = features._transform_input(x)
-        f = features(x)
-        fs += [f.detach()]
+        fs += [features_batch(x, features, transform)]
 
     f = torch.cat(fs, dim=0)
-    mean = f.mean(dim=0)
-    cov = f.T.cov()
+    return mean_and_cov(f)
 
-    return mean.detach().cpu().numpy(), cov.detach().cpu().numpy()
-
-
-def fid(x_real: np.ndarray, x_gen: np.ndarray, eps = 1e-6) -> float:
-
-    features = inception_v3(pretrained=True)
-    features.dropout = nn.Sequential()
-    features.fc = nn.Sequential()
-    features = features.cuda()
-    features.eval()
-
-    transform = nn.Sequential(
-        nn.AdaptiveAvgPool2d([299, 299]),
-    )
-
-    x_real.resize([x_real.shape[0], 3, 64, 64])
-    x_gen .resize([x_gen .shape[0], 3, 64, 64])
-
-    mean_real, cov_real = features_mean_and_cov(x_real, features, transform)
-    mean_gen , cov_gen  = features_mean_and_cov(x_gen , features, transform)
+def fid_gaussian(
+    mean_real: np.ndarray, cov_real: np.ndarray,
+    mean_gen: np.ndarray, cov_gen: np.ndarray
+) -> float:
 
     # time-consuming part
     debug_print("Calculating matrix sqrt...")
@@ -75,3 +72,29 @@ def fid(x_real: np.ndarray, x_gen: np.ndarray, eps = 1e-6) -> float:
     cov_discrepancy = np.trace(cov_real) + np.trace(cov_gen) - 2 * np.trace(covmean)
 
     return float(mean_discrepancy + cov_discrepancy)
+
+def build_features_inception_v3():
+
+    features = inception_v3(pretrained=True)
+    features.dropout = nn.Sequential()
+    features.fc = nn.Sequential()
+    features = features.cuda()
+    features.eval()
+
+    transform = nn.Sequential(
+        nn.AdaptiveAvgPool2d([299, 299]),
+    )
+
+    return features, transform
+
+def fid(x_real: np.ndarray, x_gen: np.ndarray, eps = 1e-6) -> float:
+
+    features, transform = build_features_inception_v3()
+
+    x_real.resize([x_real.shape[0], 3, 64, 64])
+    x_gen .resize([x_gen .shape[0], 3, 64, 64])
+
+    mean_real, cov_real = features_mean_and_cov(x_real, features, transform)
+    mean_gen , cov_gen  = features_mean_and_cov(x_gen , features, transform)
+
+    return fid_gaussian(mean_real, cov_real, mean_gen, cov_gen)
