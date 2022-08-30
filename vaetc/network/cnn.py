47b1e8c5f4ebd11c0d5d15db1683161cc24bbbcc
Ganmodokix <lugerp07@live.jp>
2022-08-30 16:23:34 +0900

    revert cnn.py

--- vaetc/network/cnn.py
+++ vaetc/network/cnn.py
@@ -3,52 +3,49 @@ from torch import nn
 from torch.nn import functional as F
 from .blocks import SigmoidInverse, ResBlock
 
-# STANDARD_ARCHITECTURE = [32, 64, 128, 256]
-STANDARD_ARCHITECTURE = [128, 256, 512, 1024]
-
 class ConvEncoder(nn.Module):
     """ A convolutional encoder, the same as :class:`ConvGaussianEncoder` without logvar """
 
-    def __init__(self,
-        z_dim: int, in_features: int = 3,
-        batchnorm: bool = True, batchnorm_momentum: float = 0.1,
-        inplace: bool = True, resblock: bool = False,
-        hidden_filters: list[int] = STANDARD_ARCHITECTURE
-    ):
+    def __init__(self, z_dim: int, in_features: int = 3, batchnorm: bool = True, inplace: bool = True, resblock: bool = False):
 
         super().__init__()
 
         self.z_dim = int(z_dim)
         self.in_features = int(in_features)
 
-        assert len(hidden_filters) == 4
+        padding_mode = "zeros" if torch.are_deterministic_algorithms_enabled() else "replicate"
 
-        padding_mode = "zeros"
-
-        layers_conv = []
-        layers_conv += [
-            nn.Conv2d(self.in_features, hidden_filters[0], 4, stride=2, padding=1, padding_mode=padding_mode),
-            nn.LeakyReLU(0.2, inplace),
-            nn.BatchNorm2d(hidden_filters[0], momentum=batchnorm_momentum) if batchnorm else None,
-            ResBlock(hidden_filters[0], batchnorm=batchnorm, batchnorm_momentum=batchnorm_momentum) if resblock else None,
-            ResBlock(hidden_filters[0], batchnorm=batchnorm, batchnorm_momentum=batchnorm_momentum) if resblock else None,
-        ]
-        for in_filters, out_filters in zip(hidden_filters[:-1], hidden_filters[1:]):
-            layers_conv += [
-                nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1, padding_mode=padding_mode),
-                nn.LeakyReLU(0.2, inplace),
-                nn.BatchNorm2d(out_filters, momentum=batchnorm_momentum) if batchnorm else None,
-                ResBlock(out_filters, batchnorm=batchnorm, batchnorm_momentum=batchnorm_momentum) if resblock else None,
-                ResBlock(out_filters, batchnorm=batchnorm, batchnorm_momentum=batchnorm_momentum) if resblock else None,
-            ]
+        layers = [
+            SigmoidInverse(),
             
-        layers_fc = [
-            nn.Flatten(),
-            nn.Linear(hidden_filters[-1] * 4 * 4, 256),
-            nn.LeakyReLU(0.2, inplace),
-        ]
+            nn.Conv2d(self.in_features, 32, 4, stride=2, padding=1, padding_mode=padding_mode),
+            nn.SiLU(inplace),
+            nn.BatchNorm2d(32) if batchnorm else None,
+            ResBlock(32, batchnorm=batchnorm) if resblock else None,
+            ResBlock(32, batchnorm=batchnorm) if resblock else None,
 
-        layers = [SigmoidInverse()] + layers_conv + layers_fc
+            nn.Conv2d(32, 64, 4, stride=2, padding=1, padding_mode=padding_mode),
+            nn.SiLU(inplace),
+            nn.BatchNorm2d(64) if batchnorm else None,
+            ResBlock(64, batchnorm=batchnorm) if resblock else None,
+            ResBlock(64, batchnorm=batchnorm) if resblock else None,
+            
+            nn.Conv2d(64, 128, 4, stride=2, padding=1, padding_mode=padding_mode),
+            nn.SiLU(inplace),
+            nn.BatchNorm2d(128) if batchnorm else None,
+            ResBlock(128, batchnorm=batchnorm) if resblock else None,
+            ResBlock(128, batchnorm=batchnorm) if resblock else None,
+            
+            nn.Conv2d(128, 256, 4, stride=2, padding=1, padding_mode=padding_mode),
+            nn.SiLU(inplace),
+            nn.BatchNorm2d(256) if batchnorm else None,
+            ResBlock(256, batchnorm=batchnorm) if resblock else None,
+            ResBlock(256, batchnorm=batchnorm) if resblock else None,
+            
+            nn.Flatten(),
+            nn.Linear(256 * 4 * 4, 256),
+            nn.SiLU(inplace),
+        ]
         layers = [layer for layer in layers if layer is not None]
 
         self.net = nn.Sequential(*layers)
@@ -63,26 +60,11 @@ class ConvGaussianEncoder(ConvEncoder):
     """ A convolutional encoder mainly based on the original VAE paper's settings
     [Higgins+, 2016 (https://openreview.net/forum?id=Sy2fzU9gl)] """
 
-    def __init__(self,
-        z_dim: int, in_features: int = 3,
-        batchnorm: bool = True, batchnorm_momentum: float = 0.1,
-        inplace: bool = True, resblock: bool = False,
-        hidden_filters: list[int] = STANDARD_ARCHITECTURE
-    ):
+    def __init__(self, z_dim: int, in_features: int = 3, batchnorm: bool = True, inplace: bool = True, resblock: bool = False):
 
-        super().__init__(z_dim, in_features, batchnorm, batchnorm_momentum, inplace, resblock, hidden_filters)
+        super().__init__(z_dim, in_features, batchnorm, inplace, resblock)
 
-        if batchnorm:
-            self.fc = nn.Sequential(
-                nn.Linear(256, self.z_dim),
-                nn.BatchNorm1d(self.z_dim, momentum=batchnorm_momentum),
-            )
-            self.fc_logvar = nn.Sequential(
-                nn.Linear(256, self.z_dim),
-                nn.BatchNorm1d(self.z_dim, momentum=batchnorm_momentum),
-            )
-        else:
-            self.fc_logvar = nn.Linear(256, self.z_dim)
+        self.fc_logvar = nn.Linear(256, self.z_dim)
 
     def forward(self, x: torch.Tensor):
 
@@ -97,51 +79,43 @@ class ConvDecoder(nn.Module):
     """ A convolutional decoder mainly based on the original VAE paper's settings
     [Higgins+, 2016 (https://openreview.net/forum?id=Sy2fzU9gl)] """
 
-    def __init__(self,
-        z_dim: int, out_features: int = 3,
-        batchnorm: bool = True, batchnorm_momentum: float = 0.1,
-        inplace: bool = True, resblock: bool = False,
-        hidden_filters: list[int] = STANDARD_ARCHITECTURE[::-1]
-    ):
+    def __init__(self, z_dim: int, out_features: int = 3, batchnorm: bool = True, inplace: bool = True, resblock: bool = False):
         super().__init__()
 
         self.z_dim = z_dim
         self.out_features = int(out_features)
 
-        assert len(hidden_filters) == 4
-
-        layers_fc = [
+        layers = [
             nn.Linear(self.z_dim, 256),
-            nn.LeakyReLU(0.2, inplace),
-            nn.Linear(256, hidden_filters[0] * 4 * 4),
-            nn.LeakyReLU(0.2, inplace),
-            nn.Unflatten(1, [hidden_filters[0], 4, 4]),
+            nn.SiLU(inplace),
+            nn.Linear(256, 256 * 4 * 4),
+            nn.SiLU(inplace),
+            nn.Unflatten(1, [256, 4, 4]),
+
+            nn.ConvTranspose2d(256, 128, 4, stride=2, padding=1, padding_mode="zeros"),
+            nn.SiLU(inplace),
+            nn.BatchNorm2d(128) if batchnorm else None,
+            ResBlock(128, batchnorm=batchnorm) if resblock else None,
+            ResBlock(128, batchnorm=batchnorm) if resblock else None,
+
+            nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1, padding_mode="zeros"),
+            nn.SiLU(inplace),
+            nn.BatchNorm2d(64) if batchnorm else None,
+            ResBlock(64, batchnorm=batchnorm) if resblock else None,
+            ResBlock(64, batchnorm=batchnorm) if resblock else None,
+
+            nn.ConvTranspose2d(64, 32, 4, stride=2, padding=1, padding_mode="zeros"),
+            nn.SiLU(inplace),
+            nn.BatchNorm2d(32) if batchnorm else None,
+            ResBlock(32, batchnorm=batchnorm) if resblock else None,
+            ResBlock(32, batchnorm=batchnorm) if resblock else None,
+
+            nn.ConvTranspose2d(32, self.out_features, 4, stride=2, padding=1, padding_mode="zeros"),
+            ResBlock(self.out_features, batchnorm=batchnorm) if resblock else None,
+            ResBlock(self.out_features, batchnorm=batchnorm) if resblock else None,
+
+            nn.Sigmoid(),
         ]
-
-        layers_conv = []
-        for in_filters, out_filters in zip(hidden_filters[:-1], hidden_filters[1:]):
-            
-            unit = [
-                nn.ConvTranspose2d(in_filters, out_filters, 4, stride=2, padding=1, padding_mode="zeros"),
-                nn.LeakyReLU(0.2, inplace),
-                nn.BatchNorm2d(out_filters, momentum=batchnorm_momentum) if batchnorm else None,
-                ResBlock(out_filters, batchnorm=batchnorm, batchnorm_momentum=batchnorm_momentum) if resblock else None,
-                ResBlock(out_filters, batchnorm=batchnorm, batchnorm_momentum=batchnorm_momentum) if resblock else None,
-            ]
-
-            layers_conv += unit
-            
-        last_filters = hidden_filters[-1]
-        layers_conv += [
-            nn.ConvTranspose2d(last_filters, last_filters, 4, stride=2, padding=1, padding_mode="zeros"),
-            nn.LeakyReLU(0.2, inplace),
-            nn.BatchNorm2d(last_filters, momentum=batchnorm_momentum) if batchnorm else None,
-            ResBlock(last_filters, batchnorm=batchnorm, batchnorm_momentum=batchnorm_momentum) if resblock else None,
-            ResBlock(last_filters, batchnorm=batchnorm, batchnorm_momentum=batchnorm_momentum) if resblock else None,
-            nn.Conv2d(last_filters, self.out_features, 5, 1, 2, padding_mode="zeros"),
-        ]
-
-        layers = layers_fc + layers_conv + [nn.Sigmoid()]
         layers = [layer for layer in layers if layer is not None]
 
         self.net = nn.Sequential(*layers)
